53aaa81617f27f9655536fccfcf6b2c059f82231
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-05-26 06:31:38 +0530

    Time: 4 ms (7.07%), Space: 42.2 MB (5.14%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -0,0 +1,26 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    int[] table=new int[256];
+    
+    void initialize(){
+        table[0]=0;
+        for(int i=1;i<256;i++){
+            table[i]=(i&1)+table[i/2];
+        }
+    }
+    
+    public int hammingWeight(int n) {
+        initialize();
+        int res=table[n&0xff];
+        n=n>>8;
+        
+        res=res+table[n&0xff];
+        n=n>>8;
+        
+        res=res+table[n&0xff];
+        n=n>>8;
+        
+        res=res+table[n&0xff];
+        return res;
+    }
+}
