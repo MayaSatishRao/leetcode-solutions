1977aac08e61348095f82a7f48163af76ad3131c
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-06-04 14:06:08 +0530

    Time: 200 ms (36.20%), Space: 126.3 MB (61.09%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -3,18 +3,27 @@ class NumMatrix {
     int[][] arr;
     public NumMatrix(int[][] matrix) {
         arr = new int[matrix.length][matrix[0].length];
-        for(int i=0;i<matrix.length;i++)
-            for(int j=0;j<matrix[i].length;j++)
-                arr[i][j]=matrix[i][j];
+        for(int i=0;i<matrix.length;i++){
+            arr[i][0]=matrix[i][0];
+            for(int j=1;j<matrix[0].length;j++)
+                arr[i][j]=arr[i][j-1]+matrix[i][j];
+        }
+        
+        for(int i=1;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++)
+                arr[i][j]+=arr[i-1][j];
+        }
+        
+        
+            
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++)
-            for(int j=col1;j<=col2;j++)
-                sum+=arr[i][j];
+        int top = (row1>0)?arr[row1-1][col2]:0;
+        int left = (col1>0)?arr[row2][col1-1]:0;
+        int repeat = (row1>0 && col1>0)?arr[row1-1][col1-1]:0;
         
-        return sum;
+        return arr[row2][col2]-(top+left-repeat);
     }
 }
 
