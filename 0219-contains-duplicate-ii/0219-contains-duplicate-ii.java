6b4805f3d7c5b4d4a7a89121efb1f2452ee5ac99
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-10-21 10:32:55 +0530

    Time: 58 ms (35.88%), Space: 77.6 MB (74.69%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        TreeSet<Integer> ts = new TreeSet<>();
+        HashSet<Integer> ts = new HashSet<>();
         int n = nums.length;
         
         for(int i=0;i<=k && i<n;i++){
