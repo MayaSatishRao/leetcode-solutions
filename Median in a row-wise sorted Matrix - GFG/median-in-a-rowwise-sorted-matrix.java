8c6f9f4f68fae5d993aec908222dc178206b38b3
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-11-01 19:37:39 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    int lessEl(int[] arr, int key){
+        int n = arr.length;
+        int low=0, high=n-1;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            
+            if(arr[mid]<=key)
+            low=mid+1;
+            else
+            high=mid-1;
+        }
+        
+        return low;
+    }
+    int median(int matrix[][], int R, int C) {
+        // code here      
+        int low=1, high=2000;
+        while(low<=high){
+            int mid = low+ (high-low)/2;
+             int count=0;
+             
+             for(int i=0;i<R;i++){
+                 count+=lessEl(matrix[i],mid);
+             }
+             
+             if(count>=(R*C)/2+1)
+              high=mid-1;
+             else
+              low=mid+1;
+              
+              //System.out.println(mid+" "+count);
+        }
+        
+        return low;
+    }
+}
