f317ccc42c7ee3c31ffa76c64b23847e0f7a8ac9
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-11-07 13:41:07 +0530

    Time: 14 ms (7.80%), Space: 41.7 MB (8.93%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String str = Integer.toString(num);
+        for(int i=0;i<str.length();i++)
+            if(str.charAt(i)=='6'){
+                    str = str.substring(0,i)+'9'+str.substring(i+1);
+                    break;
+            }
+        return Integer.parseInt(str);
+    }
+}
