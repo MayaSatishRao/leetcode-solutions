e95c8ac98d49ae40bac9070c305ada0262f4207b
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-11-26 10:00:57 +0530

    Added solution - LeetHub

---
+++ Construct Binary Tree from String with bracket representation - GFG/construct-binary-tree-from-string-with-bracket-representation.java
@@ -0,0 +1,172 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            String s; 
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            Node res = obj.treeFromString(s);
+            
+            Node.inorder(res);
+            System.out.println();
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static Node treeFromString(String s) {
+        // code here
+        
+        int i=0;
+        for(;i<s.length();i++)
+           if(s.charAt(i)>='0' && s.charAt(i)<='9')
+             continue;
+           else
+              break;
+        Node root = null;
+        
+        if(i>0)
+         root = new Node(Integer.parseInt(s.substring(0,i)));
+        else
+        return null;
+        
+        int open=1;
+        int leftStart=i+1,j=i+2;
+        while(j<s.length() && open!=0){
+            if(s.charAt(j)=='(')
+              open++;
+            else if(s.charAt(j)==')')
+              open--;
+              j++;
+        }
+        if(leftStart<s.length())
+          root.left = treeFromString(s.substring(leftStart,j));
+        int rightStart=j+1;
+        if(rightStart<s.length())
+          root.right=treeFromString(s.substring(rightStart));
+        
+        return root;
+    }
+}
+        
