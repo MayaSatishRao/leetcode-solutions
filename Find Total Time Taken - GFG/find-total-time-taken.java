18532a8eb3c37fd8afdf71cef3e721c802b2e4d0
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2023-04-15 18:41:45 +0530

    Added solution - LeetHub

---
+++ Find Total Time Taken - GFG/find-total-time-taken.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[]=new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for(int i=0;i<n;i++){
+                a[i]=Integer.parseInt(s[i]);
+            }
+            s=in.readLine().trim().split(" ");
+            int time[]=new int[n];
+            for(int i=0;i<n;i++){
+                time[i]=Integer.parseInt(s[i]);
+            }
+            Solution ob=new Solution();
+            long ans=ob.totalTime(n,a,time);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static long totalTime(int n,int arr[],int time[])
+    {
+        
+        HashSet<Integer> hs= new HashSet<>();
+        long res=-1;
+        
+        for(int i=0;i<n;i++){
+            if(!hs.contains(arr[i])){
+                res+=1;
+                hs.add(arr[i]);
+            }else{
+                res+=time[arr[i]-1];
+                //System.out.println(i+" "+time[arr[i]-1]);
+            }
+            
+            //System.out.println(res);
+        }
+        
+        return res;
+        
+    }
+}
