bc7fbbd9c801d79a930aab516c986ddf5219ac74
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2023-05-10 09:20:22 +0530

    Added solution - LeetHub

---
+++ Total Cuts - GFG/total-cuts.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int K;
+            K = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.totalCuts(N, K, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int totalCuts(int N, int K, int[] A) {
+        // code here
+        int[] small = new int[N];
+        small[N-1] = A[N-1];
+        
+        for(int i=N-2;i>=0;i--)
+          small[i] = Math.min(small[i+1],A[i]);
+        
+        int max = Integer.MIN_VALUE,res=0;
+        for(int i=0;i<N-1;i++){
+            max = Math.max(max,A[i]);
+            if(max+small[i+1]>=K)
+              res++;
+        }
+        
+        return res;
+    }
+}
+        
