b08e1b11829fcf3d0e1361c07b4ce54adfc0f74e
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2023-06-19 12:59:41 +0530

    Added solution - LeetHub

--- Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.java
+++ Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.java
@@ -32,32 +32,77 @@ class GFG
 
 class Solution
 {
+    public int ceilIndex(ArrayList<Integer> arr, int key){
+        int low=0, high=arr.size()-1;
+        while(low<high){
+            int mid = low+ (high-low)/2;
+            
+            if(arr.get(mid)>=key)
+              high=mid;
+            else
+              low=mid+1;
+        }
+        return high;
+    }
+    
+    public void reverse(int[] nums){
+        int n = nums.length;
+        int i=0, j=n-1;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            
+            i++;
+            j--;
+        }
+    }
     public int LongestBitonicSequence(int[] nums)
     {
         // Code here
         int n = nums.length;
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(nums[0]);
         int[] inc = new int[n];
         int[] dec = new int[n];
         
-        for(int i=0;i<n;i++){
-            inc[i] = 1;
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j])
-                  inc[i] = Math.max(inc[i], 1+inc[j]);
+        inc[0] = 1;
+        for(int i=1;i<nums.length;i++){
+            if(arr.get(arr.size()-1)<nums[i]){
+                arr.add(nums[i]);
+                inc[i] = arr.size();
+            }else{
+                
+                int index = ceilIndex(arr,nums[i]);
+                //System.out.println(index);
+                arr.set(index,nums[i]);
+                inc[i] = index+1;
             }
         }
         
-        for(int i=n-1;i>=0;i--){
-            dec[i] = 1;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j])
-                  dec[i] = Math.max(dec[i], 1+dec[j]);
+        arr = new ArrayList<>();
+        reverse(nums);
+        dec[0] = 1;
+        
+        //reverse the array
+        
+        arr.add(nums[0]);
+        
+        for(int i=1;i<nums.length;i++){
+            if(arr.get(arr.size()-1)<nums[i]){
+                arr.add(nums[i]);
+                dec[i] = arr.size();
+            }else{
+                int index = ceilIndex(arr,nums[i]);
+                arr.set(index,nums[i]);
+                dec[i] = index+1;
             }
         }
+        
         
         //System.out.println(Arrays.toString(inc));
+        reverse(dec);
         //System.out.println(Arrays.toString(dec));
-        
         int res=0;
         for(int i=0;i<n;i++){
             res = Math.max(res, inc[i]+dec[i]-1);
